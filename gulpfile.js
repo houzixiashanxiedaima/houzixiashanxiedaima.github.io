1f0b44dd58da29811a3c29b0c82a6a1e792185c6
houzixiashanxiedaima <houzixiashanxiedaima@foxmail.com>
2019-05-19 17:03:19 +0800

    new pc test3

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,48 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-// 压缩 public 目录 css
+var imagemin = require('gulp-imagemin');
+
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            removeEmptyAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyJS: true,
+            minifyCSS: true
+        }))
+        .on('error', function(err) {
+            console.log('html Error!', err.message);
+            this.end();
+        })
+        .pipe(gulp.dest('./public'))
+});
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
         .pipe(minifycss())
         .pipe(gulp.dest('./public'));
 });
-// 压缩 public 目录 html
-gulp.task('minify-html', function() {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
-});
-// 压缩 public/js 目录 js
 gulp.task('minify-js', function() {
-    return gulp.src('./public/**/*.js')
+    return gulp.src(['./public/js/**/*.js', '!./public/js/**/*.{min,mini}.js'])
+        .pipe(babel())
         .pipe(uglify())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./public/js'));
 });
-// 执行 gulp 命令时执行的任务
-gulp.task('default', [
-    'minify-html','minify-css','minify-js'
-]);
+gulp.task('minify-images', function() {
+    return gulp.src('./public/img/**/*.*')
+        .pipe(imagemin(
+        [imagemin.gifsicle({'optimizationLevel': 3}),
+        imagemin.jpegtran({'progressive': true}),
+        imagemin.optipng({'optimizationLevel': 8}),
+        imagemin.svgo()],
+        {'verbose': true}))
+        .pipe(gulp.dest('./public/img'))
+});
+gulp.task('default', gulp.parallel('minify-html','minify-css','minify-js','minify-images', function(done){
+    done();
+}));
